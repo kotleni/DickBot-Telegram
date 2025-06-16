abe7fda4b3f5f2928b0fc201f4c2c3c2ce16c7d2
Viktor Varenik <yavarenikya@gmail.com>
2025-06-16 18:18:22 +0300

    feat: new rangs system

--- src/player.ts
+++ src/player.ts
@@ -13,6 +13,45 @@ function createPlayer(data: PlayerData, tgId: string): Player {
     return new Player(data, tgId);
 }
 
+interface Gender {
+    name: string;
+}
+
+interface GenderRule {
+    gender: Gender;
+    minSize: number;
+    maxSize: number;
+}
+
+class GenderResolver {
+    private rules: GenderRule[] = [
+        // Rules should be sorted by minSize
+        { gender: { name: "Дівчина" }, minSize: 0, maxSize: 0 },
+        { gender: { name: "Фембой" }, minSize: 1, maxSize: 10 },
+        { gender: { name: "Хлопець" }, minSize: 10, maxSize: 44 },
+        { gender: { name: "Мастурбатор" }, minSize: 44, maxSize: 70 },
+        { gender: { name: "Чоловік" }, minSize: 70, maxSize: 100 },
+        { gender: { name: "Анальний дебошир" }, minSize: 100, maxSize: 200 },
+        { gender: { name: "Секс-монстер" }, minSize: 200, maxSize: 300 },
+        { gender: { name: "Насильник" }, minSize: 300, maxSize: 400 },
+        { gender: { name: "БДСМ-Катувальник" }, minSize: 400, maxSize: 666 },
+        { gender: { name: "HOMELANDER" }, minSize: 666, maxSize: 1000 },
+        { gender: { name: "Сукуб-осіменитель" }, minSize: 1000, maxSize: 3000 },
+        {
+            gender: { name: "Бог сексу" },
+            minSize: 3000,
+            maxSize: 9999999999999999,
+        },
+    ];
+
+    forPlayer(player: Player): Gender {
+        const dickSize = player.getDickSize();
+        return this.rules.find((rule) => {
+            return dickSize >= rule.minSize && dickSize <= rule.maxSize;
+        })?.gender!!;
+    }
+}
+
 class Player {
     private data: PlayerData;
     private tgId: string;
@@ -36,17 +75,8 @@ class Player {
     }
 
     getPlayerGender(): string {
-        const dickSize = this.getDickSize();
-        if (dickSize < 2) return "Дівчина";
-        if (dickSize < 10) return "Фембой";
-        if (dickSize > 70) return "Секс-монстер";
-        if (dickSize > 333) return "HOMELANDER";
-
-        const score = this.getScore();
-        if (score < 2) return "Хлопчик";
-        if (score < 66) return "Хлопець";
-        if (score < 999) return "Чоловік";
-        return "Дід";
+        const resolver = new GenderResolver();
+        return resolver.forPlayer(this).name;
     }
 
     getUsername(): string {
